f1c8886534f74b41d78c0bd8efbd1cf588404b29
Антон Швец <a.shvec@santehnika-tut.ru>
2022-03-04 20:39:10 +0300

    Оформлено навигационное меню

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import NavMenu from "./components/NavMenu/NavMenu";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavMenu />
+      <Switch>
+
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>
+            This is ContactPages
+        </h1>
+    </div>
+  )
+}
