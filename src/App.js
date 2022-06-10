f6b3a7fc07e862d5bc58864e7b3586ad4497ee5d
Anton <antonshvetswork@yandex.ru>
2022-06-10 23:44:23 +0300

    add scrollToTop

--- src/App.js
+++ src/App.js
@@ -5,27 +5,31 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
-    <Router>
-      <NavMenu />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <NavMenu />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
